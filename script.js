1546d99b1973428f2b4fb494c74ffeb82f50face
Nikita <ndemidovn@gmail.com>
2022-07-11 16:44:37 +0300

    start game

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const runInput = document.querySelector('#run_input');
-
-const line = 'Write some text here...';
-const speed = 150;
-
-
-  // runInput.value = line.substring(0, i)
-
-  let i = 0;
-
-
-  function runLine(){
-    if (i++ < line.length){
-      runInput.value = line.substring(0, i)
-    }
-    else {
-      runInput.value = ''
-      i = 0
-    }
-    done = setTimeout('runLine()', speed)
+function startGame(){
+  gameLoop();
+}
+  let loops = 0;
+  let peopleVisible = false;
+ function gameLoop(){
+  peopleVisible = !peopleVisible;
+  loops++;
+  if (loops < 12){
+    setTimeout(gameLoop, 3000);
   }
-  runLine();
-
+  else{
+    alert('Игра закончена!');
+  }
+ }
+ 
