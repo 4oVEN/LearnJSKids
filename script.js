8e023f68208b84206facac88a38b5d88302fc6e2
Nikita <ndemidovn@gmail.com>
2022-07-11 23:53:08 +0300

    mid game

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ function startGame(){
 }
   let loops = 0;
   let peopleVisible = false;
- function gameLoop(){
-  peopleVisible = !peopleVisible;
-  loops++;
-  if (loops < 12){
-    setTimeout(gameLoop, 3000);
-  }
-  else{
-    alert('Игра закончена!');
-  }
+  let gameScore = 0;
+
+  function gameLoop(){
+    peopleVisible = !peopleVisible;
+    createCharacters();
+    loops++;
+    if (loops < 12){
+      setTimeout(gameLoop, 3000);
+    }
+    else{
+      alert('Твой счет ' + gameScore);
+    }
  }
+  function createCharacters(){
+    let board = document.getElementById('board');
+    let classToSet = peopleVisible ? 'character visible' : 'character hidden';
+    for (let i = 0; i < 6; i++) {
+      board.children[i].className = classToSet; 
+      board.children[i].innerHTML = 'Гость';
+      board.children[i].onclick = function(){
+        gameScore += -2;
+      }
+    }
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    board.children[randomNumber-1].innerHTML = 'Вор';
+    board.children[randomNumber-1].onclick = function(){
+      gameScore++;
+    }
+  }
  
